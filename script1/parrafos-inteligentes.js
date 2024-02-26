26594657c1a1c598291b7a141365d013575781fb
fabian_betancourt <al22760578@ite.edu.mx>
2024-02-26 09:58:40 -0800

    script

--- script1/parrafos-inteligentes.js
+++
@@ -1,7 +0,0 @@
-for (var i = 0; i < 10; i++) {
-    const parrafo = document.createElement('p')
-    container.style.height = 100
-    container.style.backgroundColor = randomColors[i]
-    document.body.appendChild(div)
-    document.div.appendChild(parrafo)
-  }
